fefc4c9c6a3e0168abf37710ce8f5725424131de
Pontus Alexander <pontus.alexander@gmail.com>
2018-11-26 22:36:18 +0100

    Provide first name search.

--- src/main/java/se/inoviagroup/api/PersonRepository.java
+++ src/main/java/se/inoviagroup/api/PersonRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends MongoRepository<Person, String> {
-
+    List<Person> findByFirstName(@Param("name") String name);
     List<Person> findByLastName(@Param("name") String name);
-
 }
